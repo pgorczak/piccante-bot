51e280dc7c438b6e2c3b2795d9cb3da228ca1534
Philipp Gorczak <p.gorczak@gmail.com>
2016-03-03 10:40:20 +0100

    Use unicode regexes (umlauts in month).

--- piccante_bot.py
+++ piccante_bot.py
@@ -12,8 +12,8 @@ import requests
 menu_url = ('http://www.s-bar.de/ihr-betriebsrestaurant/'
             'aktuelle-speiseplaene.html')
 
-day_regex = r'\s?\w+,\ (\d\d)\.\s\w+\s20\d\d\s'
-additives_regex = r'\s\([A-Z0-9,\s]+\)\s?'
+day_regex = re.compile(r'\s?\w+,\ (\d\d)\.\s\w+\s20\d\d\s', re.UNICODE)
+additives_regex = re.compile(r'\s\([A-Z0-9,\s]+\)\s?', re.UNICODE)
 
 
 # Grabbed directly from itertools recipes
@@ -51,13 +51,13 @@ if __name__ == '__main__':
         lambda s: 'Die aktuelle Speisekarten' not in s,
         menu_parent.stripped_strings))
     # Extract day and dishes into dict
-    days_dishes = grouper(re.split(day_regex, menu_string)[1:], 2)
+    days_dishes = grouper(day_regex.split(menu_string)[1:], 2)
     menu = {int(day): dishes for day, dishes in days_dishes}
     # Get today's dishes
     today = datetime.datetime.today()
     dishes_string = menu[today.day]
     # Separate using additives list
-    dishes = re.split(additives_regex, dishes_string)[:-1]
+    dishes = additives_regex.split(dishes_string)[:-1]
     message = format_menu(today, dishes)
     # Post
     r = requests.post(args.hook, data={'message': message})
