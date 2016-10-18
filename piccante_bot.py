15cd7f269e18ac707a311c426d9ba39cfd121a45
philipp <p.gorczak@gmail.com>
2016-10-18 13:22:57 +0200

    Include daily menu.

--- piccante_bot.py
+++ piccante_bot.py
@@ -13,8 +13,12 @@ PY2 = sys.version_info[0] == 2
 menu_url = ('http://www.s-bar.de/ihr-betriebsrestaurant/'
             'aktuelle-speiseplaene.html')
 
+daily_url = ('http://www.s-bar.de/ihr-betriebsrestaurant/'
+             'aktuelle-speiseplaene/piccante-tagesangebot.html')
+
 day_regex = re.compile(r'\s?\w+,\ (\d\d)\.\s\w+\s20\d\d\s', re.UNICODE)
 additives_regex = re.compile(r'\s\([A-Z0-9,\s]+\)\s?', re.UNICODE)
+daily_regex = re.compile(r'\s(F\.|S\.R\.).*?\s', re.UNICODE)
 
 
 def zip_longest(*args, **kwargs):
@@ -41,7 +45,8 @@ def format_menu(date, dishes):
     return menu
 
 
-def post(hook_url):
+def weekly(date):
+    """ Get list of dishes from weekly menu. """
     # Get and parse html
     req = requests.get(menu_url)
     soup = bs4.BeautifulSoup(req.text, 'html.parser')
@@ -55,14 +60,27 @@ def post(hook_url):
     # Extract day and dishes into dict
     days_dishes = grouper(day_regex.split(menu_string)[1:], 2)
     menu = {int(day): dishes for day, dishes in days_dishes}
-    # Get today's dishes
-    today = datetime.datetime.today()
-    dishes_string = menu[today.day]
-    # Separate using additives list
-    dishes = additives_regex.split(dishes_string)[:-1]
-    message = format_menu(today, dishes)
-    # Post
-    return requests.post(hook_url, data=json.dumps({'text': message}))
+    # Get day's dishes and separate using additives list
+    return additives_regex.split(menu[date.day])[:-1]
+
+
+def daily():
+    """ Get list of dishes from daily menu. """
+    req = requests.get(daily_url)
+    soup = bs4.BeautifulSoup(req.text, 'html.parser')
+    # Find daily dishes section
+    menu_parent = soup.find(id='c937').find(class_="csc-textpic-text")
+    # Again deal with crufty html
+    daily_string = ' '.join(menu_parent.stripped_strings)
+    # Separate via unknown abbreviated info and additives
+    dishes = daily_regex.split(daily_string+' ')[:-1]
+    return dishes[::2]
+
+
+def post(hook_url):
+    today = datetime.datetime.now()
+    menu = format_menu(today, weekly(today) + daily())
+    return requests.post(hook_url, data=json.dumps({'text': menu}))
 
 
 if __name__ == '__main__':
